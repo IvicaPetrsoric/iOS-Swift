5daaf01c7c9d9d2f2c98066e59df2b43780557d8
ivica petrsoric <ivica.petrsoric@codecacao.com>
2019-06-21 13:27:15 +0200

    implemented drawing on canvas

--- iOS Swift/ViewController.swift
+++ iOS Swift/ViewController.swift
@@ -9,13 +9,109 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let canvas = Canvas()
+    
+    let undoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Undo", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(handleUndo), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc fileprivate func handleUndo() {
+        canvas.undo()
+    }
+    
+    let clearButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Clear", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(handleClear), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc fileprivate func handleClear() {
+        canvas.clear()
+    }
+    
+    let yellowButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .yellow
+        button.layer.borderWidth = 1
+        button.addTarget(self, action: #selector(handleColorChange), for: .touchUpInside)
+        return button
+    }()
+    
+    let redButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .red
+        button.layer.borderWidth = 1
+        button.addTarget(self, action: #selector(handleColorChange), for: .touchUpInside)
+        return button
+    }()
+    
+    let blueButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .blue
+        button.layer.borderWidth = 1
+        button.addTarget(self, action: #selector(handleColorChange), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc fileprivate func handleColorChange(button: UIButton) {
+        canvas.setStrokeColor(color: button.backgroundColor!)
+    }
+    
+    let slider: UISlider = {
+        let slider = UISlider()
+        slider.minimumValue = 1
+        slider.maximumValue = 10
+        slider.addTarget(self, action: #selector(handleSliderChane), for: .valueChanged)
+        return slider
+    }()
+    
+    @objc fileprivate func handleSliderChane() {
+        canvas.setStrokeWidth(slider.value)
+        print(slider.value)
+    }
+    
+    override func loadView() {
+        self.view = canvas
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        setupLayout()
     }
-
-
+    
+    fileprivate func setupLayout() {
+        view.backgroundColor = .white
+        
+        //        view.addSubview(canvas)
+        //        canvas.backgroundColor = .white
+        //        canvas.frame = view.frame
+        
+        let colorStackView = UIStackView(arrangedSubviews: [yellowButton, redButton, blueButton])
+        colorStackView.distribution = .fillEqually
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            undoButton,
+            clearButton,
+            colorStackView,
+            slider
+            ])
+        
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fillEqually
+        stackView.spacing = 8
+        
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
+    }
+    
 }
-
