825a7cb6e24bf34c06c88a8411350c9c2caa4be8
ivica petrsoric <ivica.petrsoric@codecacao.com>
2019-06-21 13:31:20 +0200

    implemented chaining slide to left animations

--- iOS Swift/ViewController.swift
+++ iOS Swift/ViewController.swift
@@ -10,12 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let titleLabel = UILabel()
+    let bodyLabel = UILabel()
+    
+    
+    fileprivate func setupLabels() {
+        titleLabel.backgroundColor = .red
+        bodyLabel.backgroundColor = .green
+        
+        bodyLabel.numberOfLines = 0
+        
+        titleLabel.text = "Title"
+        bodyLabel.text = "Hello my dear friends, how are you. I hope well coz i m doing some big work for all your around"
+    }
+    
+    fileprivate func setupStackView() {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, bodyLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        
+        view.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        stackView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -100).isActive = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        setupLabels()
+        setupStackView()
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapAnimation)))
     }
-
+    
+    @objc private func handleTapAnimation() {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+            self.titleLabel.transform = CGAffineTransform(translationX: -30, y: 0)
+            
+        }) { (_) in
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                self.titleLabel.alpha = 0
+                self.titleLabel.transform.translatedBy(x: 0, y: -200)
+            })
+        }
+        
+        UIView.animate(withDuration: 0.5, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+            self.bodyLabel.transform = CGAffineTransform(translationX: -30, y: 0)
+            
+        }) { (_) in
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                self.bodyLabel.alpha = 0
+                self.bodyLabel.transform.translatedBy(x: 0, y: -200)
+            })
+        }
+    }
+    
 
 }
 
