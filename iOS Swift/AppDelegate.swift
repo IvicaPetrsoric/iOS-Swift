20cae13a88612eccf5b1427fbac9474f1120b4f5
ivica petrsoric <ivica.petrsoric@codecacao.com>
2019-06-22 12:40:57 +0200

    implemented airplanes seats segmentation

--- iOS Swift/AppDelegate.swift
+++ iOS Swift/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         window = UIWindow()
         window?.makeKeyAndVisible()
-        window?.rootViewController = ViewController()
+        window?.rootViewController = AirplaneSeatingViewController(collectionViewLayout: UICollectionViewFlowLayout())
 
         return true
     }
