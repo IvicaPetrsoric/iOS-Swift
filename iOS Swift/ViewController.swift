87ea998d977734643bd2e6877efeb68a0aae7db3
ivica petrsoric <ivica.petrsoric@codecacao.com>
2019-06-22 13:58:21 +0200

    implemented pulsating circle progress bar

--- iOS Swift/ViewController.swift
+++ iOS Swift/ViewController.swift
@@ -8,14 +8,155 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, URLSessionDownloadDelegate {
+    
+    var shapeLayer = CAShapeLayer()
+    
+    var puslingLayer: CAShapeLayer!
+    
+    let percentageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Start"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.textColor = .white
+        return label
+    }()
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
+    }
+    
+    private func setupNotificationObservers(){
+        NotificationCenter.default.addObserver(self, selector: #selector(handleEnterForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
+    }
+    
+    // kada se ode u home da se aktivira ponovo anim
+    @objc private func handleEnterForeground(){
+        animatePuslingLayer()
+    }
+    
+    private func createCircleShapeLayer(strokeColor: UIColor, fillColor: UIColor) -> CAShapeLayer{
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle:  0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
+        let layer = CAShapeLayer()
+        layer.path = circularPath.cgPath
+        layer.strokeColor = strokeColor.cgColor
+        layer.lineWidth = 10
+        layer.fillColor = fillColor.cgColor
+        layer.lineCap = CAShapeLayerLineCap.round
+        layer.position = view.center
+        return layer
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        setupNotificationObservers()
+        
+        view.backgroundColor = .black
+        
+        setupCircleLayers()
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
+    
+    private func setupPercentageLayer(){
+        view.addSubview(percentageLabel)
+        percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        percentageLabel.center = view.center
+    }
+    
+    private func setupCircleLayers(){
+        //        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle:  -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
+        //        let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle:  0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
+        puslingLayer = createCircleShapeLayer(strokeColor: UIColor.rgb(r: 66, g: 26, b: 59),
+                                              fillColor: UIColor.rgb(r: 66, g: 26, b: 59))
+        view.layer.addSublayer(puslingLayer)
+        
+        let trackLayer = createCircleShapeLayer(strokeColor: UIColor.rgb(r: 66, g: 26, b: 59), fillColor: .black)
+        view.layer.addSublayer(trackLayer)
+        
+        animatePuslingLayer()
+        
+        //        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle:  -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        shapeLayer = createCircleShapeLayer(strokeColor: UIColor.rgb(r: 185, g: 20, b: 108), fillColor: .clear)
+        shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
+        shapeLayer.strokeEnd = 0
+        view.layer.addSublayer(shapeLayer)
+    }
+    
+    private func animatePuslingLayer(){
+        let animation = CABasicAnimation(keyPath: "transform.scale")
+        
+        animation.toValue = 1.5
+        animation.duration = 0.8
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        animation.autoreverses = true
+        animation.repeatCount = Float.infinity
+        
+        puslingLayer.add(animation, forKey: "pulsing")
+    }
+    
+    @objc func handleTap(){
+        beginDownloadingFile()
+        //        animateCircle()
+    }
+    
+    let urlString = "https://firebasestorage.googleapis.com/v0/b/firestorechat-e64ac.appspot.com/o/intermediate_training_rec.mp4?alt=media&token=e20261d0-7219-49d2-b32d-367e1606500c"
+    
+    private func beginDownloadingFile() {
+        print("Attempting to download file")
+        
+        shapeLayer.strokeEnd = 0
+        
+        let configuration = URLSessionConfiguration.default
+        let operationQueue = OperationQueue()
+        let urlSession = URLSession(configuration: configuration, delegate: self, delegateQueue: operationQueue)
+        
+        guard let url = URL(string: urlString) else { return }
+        let downloadTask = urlSession.downloadTask(with: url)
+        downloadTask.resume()
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        let percentage = CGFloat(totalBytesWritten) / CGFloat(totalBytesExpectedToWrite)
+        
+        DispatchQueue.main.async {
+            self.percentageLabel.text = "\(Int(percentage * 100))%"
+            self.shapeLayer.strokeEnd = percentage
+        }
+        
+        print(percentage)
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        print("Finished downloading file")
+    }
+    
+    fileprivate func animateCircle() {
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 3
+        basicAnimation.fillMode = CAMediaTimingFillMode.forwards
+        basicAnimation.isRemovedOnCompletion = false
+        
+        shapeLayer.add(basicAnimation, forKey: "urSoBasic")
+    }
+    
+}
 
-
+extension UIColor {
+    
+    static func rgb(r: CGFloat, g: CGFloat, b: CGFloat) -> UIColor {
+        return UIColor(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+    
+    static let backgroundColor = UIColor.rgb(r: 21, g: 22, b: 33)
+    static let outlineStrokeColor = UIColor.rgb(r: 234, g: 46, b: 111)
+    static let trackStrokeColor = UIColor.rgb(r: 56, g: 25, b: 49)
+    static let pulsatingFillColor = UIColor.rgb(r: 86, g: 30, b: 63)
 }
 
