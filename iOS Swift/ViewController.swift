c63cc1c2d443c040669e26f24cc7a33e8197352d
ivica petrsoric <ivica.petrsoric@codecacao.com>
2019-06-22 14:09:09 +0200

    research input text view height update

--- iOS Swift/ViewController.swift
+++ iOS Swift/ViewController.swift
@@ -9,13 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private lazy var inputTextView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .lightGray
+        textView.isScrollEnabled = false
+        textView.font = UIFont.preferredFont(forTextStyle: .headline)
+        textView.delegate = self
+        textView.text = "Enter text to change height constraint"
+        return textView
+    }()
+    
+    fileprivate var inputTextViewHeightConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        view.addSubview(inputTextView)
+        inputTextViewHeightConstraint = inputTextView.anchor(top: nil, leading: view.leadingAnchor,
+                             bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor,
+                             size: .init(width: 0, height: 50)).height
+
+        textViewDidChange(inputTextView)
     }
+    
+}
 
-
+extension ViewController: UITextViewDelegate{
+    
+    func textViewDidChange(_ textView: UITextView) {
+        let size = CGSize(width: view.frame.width, height: .infinity)
+        let estimetedSize = textView.sizeThatFits(size)
+        
+        // #1
+//        textView.constraints.forEach { (constraint) in
+//            if constraint.firstAttribute == .height{
+//                constraint.constant = estimetedSize.height
+//            }
+//        }
+        
+        // #2
+        inputTextViewHeightConstraint.constant = estimetedSize.height
+    }
+    
 }
 
