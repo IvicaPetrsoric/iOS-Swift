a64b6f8092e65ee33d9de865d0686c9935574894
ivica petrsoric <ivica.petrsoric@codecacao.com>
2019-06-22 14:49:51 +0200

    implemented FB stream animation

--- iOS Swift/ViewController.swift
+++ iOS Swift/ViewController.swift
@@ -9,13 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
-
-
+    
+    @objc func handleTap(){
+        (0...10).forEach{ (_) in
+            genetateAnimatedViews()
+        }
+    }
+    
+    fileprivate func genetateAnimatedViews(){
+        let image = drand48() > 0.5 ? #imageLiteral(resourceName: "player_0_X1") : #imageLiteral(resourceName: "player_40_X1")
+        
+        let imageView = UIImageView(image: image)
+        let dimension = 20 + drand48() * 10  // izmedu 10 i 30, drand = 0 - 1
+        imageView.frame = CGRect(x: 0, y: 0, width: dimension, height: dimension)
+        
+        CATransaction.begin()
+        CATransaction.setCompletionBlock({
+            imageView.removeFromSuperview()
+        })
+        
+        let animation = CAKeyframeAnimation(keyPath: "position")
+        animation.path = customPath(imgWidht: dimension).cgPath
+        animation.duration = 2 + drand48() * 3
+        animation.fillMode = CAMediaTimingFillMode.forwards
+        animation.isRemovedOnCompletion = false
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        
+        imageView.layer.add(animation, forKey: nil)
+        
+        CATransaction.commit()
+        
+        view.addSubview(imageView)
+    }
+    
+    func customPath(imgWidht: Double) -> UIBezierPath{
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: -CGFloat(imgWidht/2), y: 200))
+        
+        let endPoint = CGPoint(x: UIScreen.main.bounds.width + CGFloat(imgWidht/2), y: 200)
+        
+        let randomYShift = 200 + drand48() * 300
+        
+        let cp1 = CGPoint(x: 100, y: 100 - randomYShift)
+        let cp2 = CGPoint(x: 200, y: 300 + randomYShift)
+        
+        //path.addLine(to: endPoint)
+        path.addCurve(to: endPoint, controlPoint1: cp1, controlPoint2: cp2)
+        
+        return path
+        
+    }
+    
 }
 
