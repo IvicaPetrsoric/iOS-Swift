3aac6c0d4542d0d59aefef3efb89479fcd88786f
ivica petrsoric <ivica.petrsoric@codecacao.com>
2019-06-22 18:20:00 +0200

    research BLE/Beacon emitter

--- iOS Swift/ViewController.swift
+++ iOS Swift/ViewController.swift
@@ -7,15 +7,60 @@
 //
 
 import UIKit
+import CoreLocation
+import CoreBluetooth
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CBPeripheralManagerDelegate  {
+    
+    var localBeacon: CLBeaconRegion!
+    var beaconPeripheralData: NSDictionary!
+    var peripheralManager: CBPeripheralManager!
+    
+    let beaconUUID = "95E00D69-6DDF-4DFF-A9FB-B867FDA7E837"
+    let beaconId = "region"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        initLocalBeacon()
+        navigationItem.title = "Start Beaconing! :D "
+        
+        view.backgroundColor = .lightGray
     }
-
-
+    
+    func initLocalBeacon() {
+        if localBeacon != nil {
+            stopLocalBeacon()
+        }
+        
+        let localBeaconUUID = beaconUUID
+        let localBaconMinor: CLBeaconMinorValue = 200
+        let localBeaconMajor: CLBeaconMajorValue = 250
+        
+        guard let uuid = UUID(uuidString: localBeaconUUID) else { return }
+        localBeacon = CLBeaconRegion(proximityUUID: uuid, major: localBeaconMajor, minor: localBaconMinor, identifier: beaconId)
+        
+        beaconPeripheralData = localBeacon.peripheralData(withMeasuredPower: nil)
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil, options: nil)
+    }
+    
+    func stopLocalBeacon() {
+        print("Stop Local Beacon")
+        peripheralManager.stopAdvertising()
+        peripheralManager = nil
+        beaconPeripheralData = nil
+        localBeacon = nil
+    }
+    
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        if peripheral.state == .poweredOn {
+            print("Start advertising!")
+            peripheralManager.startAdvertising(beaconPeripheralData as! [String: AnyObject])
+        } else if peripheral.state == .poweredOff {
+            print("Stop advertising!")
+            peripheralManager.stopAdvertising()
+        }
+    }
+    
 }
 
