892d59daee9d4e533980d55cc7ac225aabd41ba3
ivica petrsoric <ivica.petrsoric@codecacao.com>
2019-06-24 19:23:27 +0200

    implemented fb popUp dialog

--- iOS Swift/ViewController.swift
+++ iOS Swift/ViewController.swift
@@ -1,21 +1,133 @@
 //
 //  ViewController.swift
-//  iOS Swift
+//  FBPopUpAnim
 //
-//  Created by ivica petrsoric on 21/06/2019.
-//  Copyright © 2019 ivica petrsoric. All rights reserved.
+//  Created by ivica petrsoric on 15/07/2018.
+//  Copyright © 2018 ivica petrsoric. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let iconsContainerView: UIView = {
+        let containerView = UIView()
+        containerView.backgroundColor = .white
+        
+        let iconHeight: CGFloat = 38
+        let padding: CGFloat = 6
+        let images = [#imageLiteral(resourceName: "blue_like"), #imageLiteral(resourceName: "red_heart"), #imageLiteral(resourceName: "surprised"), #imageLiteral(resourceName: "cry_laugh"), #imageLiteral(resourceName: "cry"), #imageLiteral(resourceName: "angry")]
+        
+        
+        let arrangedSubviews = images.map({ (image) -> UIView in
+            let imageView = UIImageView(image: image)
+            imageView.layer.cornerRadius = iconHeight / 2
+            imageView.isUserInteractionEnabled = true
+            return imageView
+        })
+        
+        let stackView = UIStackView(arrangedSubviews: arrangedSubviews)
+        stackView.distribution = .fillEqually
+        stackView.spacing = 8
+        
+        stackView.layoutMargins = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        stackView.isLayoutMarginsRelativeArrangement = true
+        
+        containerView.addSubview(stackView)
+        
+        let numIcons = CGFloat(arrangedSubviews.count)
+        let width = numIcons * iconHeight + (numIcons + 1) * padding
+        
+        containerView.frame = CGRect(x: 0, y: 0, width: width, height: iconHeight + 2 * padding)
+        containerView.layer.cornerRadius = containerView.frame.height / 2
+        containerView.layer.shadowColor = UIColor(white: 0.4, alpha: 0.4).cgColor
+        containerView.layer.shadowOpacity = 0.5
+        containerView.layer.shadowOffset = CGSize(width: 0, height: 4)
+        
+        stackView.frame = containerView.frame
+        return containerView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        let label = UILabel()
+        label.text = "Long Press anywhere to get popUp!"
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.font = UIFont.boldSystemFont(ofSize: 28)
+        
+        view.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        
+        view.addSubview(label)
+        label.fillSuperview()
+        
+        setupLongPressGesgture()
+    }
+    
+    fileprivate func setupLongPressGesgture() {
+        view.addGestureRecognizer(UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress)))
+    }
+    
+    @objc func handleLongPress(gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began {
+            handleGestureBegan(gesture: gesture)
+        } else if gesture.state == .changed {
+            handleGestureChanged(gesture: gesture)
+        }
+        else if gesture.state == .ended {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                let stackView = self.iconsContainerView.subviews.first
+                
+                stackView?.subviews.forEach({ (imageView) in
+                    imageView.transform = .identity
+                })
+                
+                self.iconsContainerView.transform = self.iconsContainerView.transform.translatedBy(x: 0, y: 50)
+                self.iconsContainerView.alpha = 0
+                
+            }, completion: { (_) in
+                self.iconsContainerView.removeFromSuperview()
+            })
+        }
+    }
+    
+    fileprivate func handleGestureChanged(gesture: UILongPressGestureRecognizer) {
+        let pressedLocation = gesture.location(in: self.iconsContainerView)
+        let fixedYLocation = CGPoint(x: pressedLocation.x, y: self.iconsContainerView.frame.height / 2)
+        let hitTestView = iconsContainerView.hitTest(fixedYLocation, with: nil)
+        
+        if hitTestView is UIImageView {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                let stackView = self.iconsContainerView.subviews.first
+                
+                stackView?.subviews.forEach({ (imageView) in
+                    imageView.transform = .identity
+                })
+                
+                hitTestView?.transform = CGAffineTransform(translationX: 0, y: -50)
+            })
+        }
+    }
+    
+    fileprivate func handleGestureBegan(gesture: UILongPressGestureRecognizer) {
+        view.addSubview(iconsContainerView)
+        
+        let pressedLocation = gesture.location(in: self.view)
+        
+        let centeredX = (view.frame.width - iconsContainerView.frame.width) / 2
+        iconsContainerView.transform = CGAffineTransform(translationX: centeredX, y: pressedLocation.y)
+        iconsContainerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.iconsContainerView.alpha = 1
+            self.iconsContainerView.transform = CGAffineTransform(translationX: centeredX, y: pressedLocation.y - self.iconsContainerView.frame.height)
+        })
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
 
-
+    
 }
-
