50c54fb87242ea524451b6e98079f1e40c9b3742
ivica petrsoric <ivica.petrsoric@codecacao.com>
2019-06-24 17:45:14 +0200

    refactoring

--- iOS Swift/BarChartView.swift
+++ iOS Swift/BarChartView.swift
@@ -1,5 +1,5 @@
 //
-//  BarChartCell.swift
+//  BarChartView.swift
 //  iOS Swift
 //
 //  Created by ivica petrsoric on 24/06/2019.
@@ -7,6 +7,45 @@
 //
 
 import UIKit
+
+class BarChartView: UIView {
+    
+    let titleLabel = UILabel(text: "Focus Time", font: .systemFont(ofSize: 16, weight: .semibold), textColor: #colorLiteral(red: 0.07251366228, green: 0.19975999, blue: 0.6398154497, alpha: 1))
+    let barChartController = BarChartController(scrollDirection: .horizontal)
+
+    lazy var barsContainerView: UIView = {
+        let view = UIView()
+        view.stack(views: self.titleLabel, self.barChartController.view)
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        layer.cornerRadius = 8
+        
+        setupShadow(opacity: 0.05, radius: 8, offset: .init(width: 0, height: 12), color: .black)
+        
+        stack(views: titleLabel, barsContainerView, spacing: 12).withMargins(.init(top: 16, left: 16, bottom: 12, right: 16))
+        
+        setupBars()
+    }
+    
+    private func setupBars() {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+struct BarData {
+    let index: Int
+    let percentage: CGFloat
+    let color: UIColor
+}
 
 class BarChartCell: GenericCell<BarData> {
     
@@ -83,6 +122,38 @@ class BarChartCell: GenericCell<BarData> {
         dotViewHeightConstraint.isActive = true
         dotViewWidthConstraint.isActive = true
         dotView.centerXAnchor.constraint(equalTo: indexLabel.centerXAnchor).isActive = true
+    }
+    
+}
+
+
+class BarChartController: GenericController<BarChartCell, BarData, UICollectionReusableView>, UICollectionViewDelegateFlowLayout {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        (0..<32).forEach { (i) in
+            let randomInt = Int.random(in: 0...2)
+            var color = UIColor.red
+            if randomInt == 1 {
+                color = .green
+            } else if randomInt == 2 {
+                color = .blue
+            }
+            let random = CGFloat.random(in: 0..<1)
+            items.append(BarData.init(index: i, percentage: random, color: color))
+        }
+        
+        collectionView.reloadData()
+        collectionView.backgroundColor = .clear
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: 14, height: view.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 2
     }
     
 }
