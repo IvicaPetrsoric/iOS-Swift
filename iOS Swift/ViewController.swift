09fb69bc3c95a95b5c3c54fa14cac571d892b8e3
ivica petrsoric <ivica.petrsoric@codecacao.com>
2019-06-25 15:24:36 +0200

    researching MVVM with top movies

--- iOS Swift/ViewController.swift
+++ iOS Swift/ViewController.swift
@@ -1,20 +1,44 @@
 //
 //  ViewController.swift
-//  iOS Swift
+//  TopMovie
 //
-//  Created by ivica petrsoric on 21/06/2019.
-//  Copyright © 2019 ivica petrsoric. All rights reserved.
+//  Created by ivica petrsoric on 24/06/2018.
+//  Copyright © 2018 ivica petrsoric. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    let cellId = "cell"
+    @IBOutlet var viewModel: ViewModel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        viewModel.fetchMovies {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfItemsInSection(section: section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as UITableViewCell
+        configureCell(cell: cell, forRowAtIndexPath: indexPath)
+        return cell
+    }
+    
+    func configureCell(cell: UITableViewCell, forRowAtIndexPath indexPath: IndexPath) {
+        cell.textLabel?.text = viewModel.titleForItemAtIndexPath(indexPath: indexPath)
+    }
+
 
 
 }
