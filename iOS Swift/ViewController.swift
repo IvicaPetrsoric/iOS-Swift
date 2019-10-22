c08385e16d689b7e1ec7d4f8df80d5c62301117e
ivica petrsoric <ivica.petrsoric@codecacao.com>
2019-10-22 22:09:20 +0200

    testing animations

--- iOS Swift/ViewController.swift
+++ iOS Swift/ViewController.swift
@@ -8,14 +8,143 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TabBarController: UITabBarController {
+    
+    let testButton = AnimatedButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        delegate = self
+        
+        testButton.backgroundColor = .yellow
+        
+        testButton.cornerRadius = 16
+        
+        view.addSubview(testButton)
+        testButton.frame = CGRect(x: 50, y: 150, width: 100, height: 44)
+        
+        viewControllers = [
+            generateNavigationController(with: UIViewController(), title: "Search", image: UIImage()),
+            generateNavigationController(with: UIViewController(), title: "Favorite", image: UIImage()),
+            generateNavigationController(with: UIViewController(), title: "Download", image: UIImage())
+        ]
+    }
+    
+    // MARK:- Helper function
+    fileprivate func generateNavigationController(with rootViewController: UIViewController, title: String, image: UIImage) -> UINavigationController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        return navController
     }
 
 
 }
+
+public extension UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get { return layer.cornerRadius }
+        
+        set (cornredRadius) {
+            layer.masksToBounds = true
+            layer.cornerRadius = cornredRadius
+        }
+    }
+    
+}
+
+class AnimatedButton: UIButton {
+    
+    override var isHighlighted: Bool {
+        didSet {
+            let transform: CGAffineTransform = isHighlighted ? .init(scaleX: 0.95, y: 0.95) : .identity
+            animate(transform)
+        }
+    }
+}
+
+private extension AnimatedButton {
+    
+    private func animate(_ transform: CGAffineTransform) {
+        UIView.animate(
+            withDuration: 0.4,
+            delay: 0,
+            usingSpringWithDamping: 0.5,
+            initialSpringVelocity: 3,
+            options: [.curveEaseInOut],
+            animations: {
+                self.transform = transform
+            }
+        )
+    }
+    
+}
+
+extension TabBarController: UITabBarControllerDelegate {
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        guard
+            let tabViewControllers = tabBarController.viewControllers,
+            let targetIndex = tabViewControllers.firstIndex(of: viewController),
+            let targetView = tabViewControllers[targetIndex].view,
+            let currentViewController = selectedViewController,
+            let currentIndex = tabViewControllers.firstIndex(of: currentViewController)
+            else { return false }
+        
+        if currentIndex != targetIndex {
+            animateToView(targetView, at: targetIndex, from: currentViewController.view, at: currentIndex)
+        }
+        
+        return true
+    }
+    
+}
+
+private extension TabBarController {
+    
+    func animateToView(_ toView: UIView, at toIndex: Int, from fromView: UIView, at fromIndex: Int) {
+        // Position toView off screen (to the left/right of fromView)
+        let screenWidth = UIScreen.main.bounds.size.width
+        let offset = toIndex > fromIndex ? screenWidth : -screenWidth
+        
+        toView.frame.origin = CGPoint(
+            x: toView.frame.origin.x + offset,
+            y: toView.frame.origin.y
+        )
+        
+        fromView.superview?.addSubview(toView)
+        
+        // Disable interaction during animation
+        view.isUserInteractionEnabled = false
+        
+        UIView.animate(
+            withDuration: 0.5,
+            delay: 0.0,
+            usingSpringWithDamping: 0.75,
+            initialSpringVelocity: 0.5,
+            options: .curveEaseInOut,
+            animations: {
+                // Slide the views by -offset
+                fromView.center = CGPoint(x: fromView.center.x - offset, y: fromView.center.y)
+                toView.center = CGPoint(x: toView.center.x - offset, y: toView.center.y)
+        },
+            completion: { _ in
+                // Remove the old view from the tabbar view.
+                fromView.removeFromSuperview()
+                self.selectedIndex = toIndex
+                self.view.isUserInteractionEnabled = true
+        }
+        )
+    }
+    
+}
+
+
+
 
