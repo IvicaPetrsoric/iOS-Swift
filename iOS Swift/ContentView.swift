404bbc1db459af7b11d282db8356bd5bfa17c444
ivica petrsoric <ivica.petrsoric@codecacao.com>
2020-02-09 18:26:13 +0100

    SwiftUI: Testing stacking X, Y, Z

--- iOS Swift/ContentView.swift
+++ iOS Swift/ContentView.swift
@@ -10,7 +10,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            Color.green.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    Text("LBTA").padding(.all, 16).background(Color.purple)
+                    Spacer().frame(height: 5).background(Color.red).padding(.leading, 8)
+                }
+                Spacer().frame(width: 5).background(Color.blue)
+                HStack {
+                    Spacer().frame(height: 5).background(Color.red).padding(.trailing, 8)
+                    Text("LBTA").padding(.all, 16).background(Color.purple)
+                }
+                
+            }.background(Color.yellow)
+        }
+
+        
+        
+//        HStack {
+//            Text("LBTA")
+//            Spacer()
+//                .frame(height: 5).background(Color.green)
+//            Text("LBTA")
+//                .background(Color.red)
+//        }.background(Color.blue)
+                
+//        VStack {
+//            Text("LBTA").background(Color.red)
+//            Spacer().frame(width: 10).background(Color.orange)
+//            Text("LBTA LBTA").background(Color.red)
+//        }.background(Color.blue)
     }
 }
 
