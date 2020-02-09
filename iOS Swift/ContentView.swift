4743762236ee1032c5d3e2926d257c3b9b6f17b5
ivica petrsoric <ivica.petrsoric@codecacao.com>
2020-02-09 19:28:20 +0100

    testing fetching

--- iOS Swift/ContentView.swift
+++ iOS Swift/ContentView.swift
@@ -6,11 +6,58 @@
 //  Copyright © 2020 ivica petrsoric. All rights reserved.
 //
 
+let apiUrl = "https://api.letsbuildthatapp.com/static/courses.json"
+
 import SwiftUI
 
+struct Course: Identifiable, Decodable {
+    
+    let id = UUID()
+    let name: String
+    
+}
+
+class CoursesViewModel: ObservableObject {
+    
+    @Published var messages = "Messages inside obserable object"
+    
+    @Published var courses: [Course] = [
+    .init(name: "Course 1"),
+    .init(name: "Course 2"),
+    ]
+    
+    func fetchCourses() {
+        guard let url = URL(string: apiUrl) else { return }
+        URLSession.shared.dataTask(with: url) { (data, resp, err) in
+            DispatchQueue.main.async {
+                self.courses = try! JSONDecoder().decode([Course].self, from: data!)
+            }
+        }.resume()
+    }
+    
+}
+
 struct ContentView: View {
+    
+    @ObservedObject var coursesVM = CoursesViewModel()
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            ScrollView {
+                Text(coursesVM.messages)
+                
+                ForEach(coursesVM.courses) { course in
+                    Text(course.name)
+                }
+        }.navigationBarTitle("Courses")
+        .navigationBarItems(trailing: Button(action: {
+                print("Fetching json data")
+            self.coursesVM.messages = "Something else"
+            self.coursesVM.fetchCourses()
+            }, label: {
+                Text("Fetch Courses")
+            }))
+        }
     }
 }
 
