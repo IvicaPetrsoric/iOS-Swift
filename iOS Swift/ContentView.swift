37cd95c752c87ac6acfc7a316a10e437d0df263d
ivica petrsoric <ivica.petrsoric@codecacao.com>
2020-02-09 18:35:22 +0100

    SwiftUI: simple calculator

--- iOS Swift/ContentView.swift
+++ iOS Swift/ContentView.swift
@@ -8,14 +8,130 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello World")
+enum CalculatorButton: String {
+    
+    case zero, one, two, three, four, five, six, seven, eight, nine
+    case equals, plus, minus, multiplay, divide
+    case decimal
+    case ac, plusMinus, percent
+    
+    var title: String {
+        switch self {
+        case .zero: return "0"
+        case .one: return "1"
+        case .two: return "2"
+        case .three: return "3"
+        case .four: return "4"
+        case .five: return "5"
+        case .six: return "6"
+        case .seven: return "7"
+        case .eight: return "8"
+        case .nine: return "9"
+        case .plus: return "+"
+        case .minus: return "-"
+        case .multiplay: return "X"
+        case .plusMinus: return "+/-"
+        case .percent: return "%"
+        case .divide: return "/"
+        case .equals: return "="
+        case .decimal: return "."
+        default: return "AC"
+        }
     }
+    
+    var background: Color {
+        switch self {
+        case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .decimal:
+            return Color(.darkGray)
+        case .ac, .plusMinus, .percent:
+            return Color(.lightGray)
+        default:
+            return .orange
+        }
+    }
+    
+}
+
+class GlobalEnvironment: ObservableObject {
+    
+    @Published var display = "0"
+    
+    func receiveInput(calculatorButton: CalculatorButton) {
+        self.display = calculatorButton.title
+    }
+    
+}
+
+struct ContentView: View {
+    
+    @EnvironmentObject var env: GlobalEnvironment
+    
+    let buttons: [[CalculatorButton]] = [
+        [.ac, .plusMinus, .percent, .divide],
+        [.seven, .eight, .nine, .multiplay],
+        [.four, .five, .six, .minus],
+        [.one, .two, .three, .plus],
+        [.zero, .decimal, .equals]
+    ]
+    
+    var body: some View {
+        
+        ZStack (alignment: .bottom) {
+            Color.black.edgesIgnoringSafeArea(.all)
+
+            VStack (spacing: 12) {
+                HStack {
+                    Spacer()
+                    Text(env.display).foregroundColor(.white)
+                        .font(.system(size: 64))
+                }.padding()
+                
+                ForEach(buttons, id: \.self) { row in
+                    HStack (spacing: 12) {
+                        ForEach (row, id: \.self) { button in
+                            CalculatorButtonView(button: button)
+                        }
+                    }
+                }
+            }.padding(.bottom)
+        }
+    }
+    
+
+}
+
+struct CalculatorButtonView: View {
+    
+    var button: CalculatorButton
+    
+    @EnvironmentObject var env: GlobalEnvironment
+    
+    var body: some View {
+        Button(action: {
+            self.env.receiveInput(calculatorButton: self.button)
+        }) {
+            Text(button.title)
+                .font(.system(size: 32))
+                .frame(width: self.buttonWidth(button: button), height: (UIScreen.main.bounds.width  - 5 * 12) / 4)
+                .foregroundColor(Color.white)
+                .background(button.background)
+                .cornerRadius(self.buttonWidth(button: button) / 2)
+        }
+    }
+    
+    private func buttonWidth(button: CalculatorButton) -> CGFloat {
+        if button == .zero {
+            return (UIScreen.main.bounds.width  - 4 * 12) / 4 * 2
+        } else {
+            return (UIScreen.main.bounds.width  - 5 * 12) / 4
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(GlobalEnvironment())
     }
 }
+
