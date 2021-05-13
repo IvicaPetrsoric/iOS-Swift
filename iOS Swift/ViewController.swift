9285f2297fd84d17ffc024ff1a055e543d7e888a
Ivica Petrsoric <ivica.petrsoric@gmail.com>
2021-05-13 10:55:09 +0100

    form

--- iOS Swift/ViewController.swift
+++ iOS Swift/ViewController.swift
@@ -10,15 +10,57 @@ import SwiftUI
 
 struct StartView: View {
     
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var birthDate = Date()
+    @State private var shouldSendNewsletter = false
+    @State private var numberOfLikes = 1
+    
     var body: some View {
-        VStack {
-            Color.yellow
+        NavigationView {
+            Form {
+                Section(header: Text("Personal information")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    DatePicker("Birthdate", selection: $birthDate, displayedComponents: .date)
+                }
+                
+                if #available(iOS 14.0, *) {
+                    Section(header: Text("Actions")) {
+                        Toggle("Send Newsletter", isOn: $shouldSendNewsletter)
+                            .toggleStyle(SwitchToggleStyle(tint: .red))
+                        Stepper("Number of likes", value: $numberOfLikes, in: 1...100)
+                        Text("This video has \(numberOfLikes) likes")
+                        Link("Terms of service", destination: URL(string: "htpps://www.google.com")!)
+                    }
+                    
+                    .accentColor(.red)
+                    .navigationTitle("Account")
+                    .toolbar {
+                        ToolbarItemGroup(placement: .navigationBarTrailing) {
+                            Button {
+                                hideKeyboard()
+                            } label: {
+                                Image(systemName: "keyboard.chevron.compact.down")
+                            }
+                        }
+                    }
+                } else {
+                    // Fallback on earlier versions
+                }
+            }
         }
     }
     
 }
 
-
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
 
 
 struct StartView_Previews: PreviewProvider {
