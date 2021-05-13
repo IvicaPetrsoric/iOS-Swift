27aab9bf08b44614fae3b2ec3afeac8f1515138a
Ivica Petrsoric <ivica.petrsoric@gmail.com>
2021-05-13 14:30:20 +0100

    testing-combine: setup

--- iOS Swift/ViewController.swift
+++ iOS Swift/ViewController.swift
@@ -7,12 +7,80 @@
 //
 
 import SwiftUI
+import Combine
+
+struct User: Decodable, Identifiable {
+    let id: Int
+    let name: String
+}
+
+final class ViewModel: ObservableObject {
+    
+    @Published var time = ""
+//    private var ancCancallable: AnyCancellable?
+    private var cancallables = Set<AnyCancellable>()
+    
+    @Published var users: [User] = []
+    
+    let formatter: DateFormatter = {
+        let df = DateFormatter()
+        df.timeStyle = .medium
+        return df
+    }()
+    
+    init() {
+        setupPublisher()
+    }
+    
+    private func setupPublisher() {
+        setupTimerPublisher()
+        setupDataTaskPublisher()
+    }
+    
+    private func setupTimerPublisher() {
+//        ancCancallable = Timer.publish(every: 1, on: .main, in: .default)
+        Timer.publish(every: 1, on: .main, in: .default)
+            .autoconnect()
+            .receive(on: RunLoop.main)
+            .sink { value in
+                self.time = self.formatter.string(from: value)
+            }
+            .store(in: &cancallables)
+    }
+    
+    private func setupDataTaskPublisher() {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/users")!
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap { (data, response) -> Data  in
+                guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                    throw URLError(URLError.badServerResponse)
+                }
+                
+                return data
+            }
+            .decode(type: [User].self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { _ in }) { (users) in
+                self.users = users
+            }
+            .store(in: &cancallables)
+    }
+    
+}
 
 struct StartView: View {
     
+    @StateObject var viewModel = ViewModel()
+    
     var body: some View {
         VStack {
-            Color.yellow
+            Text(viewModel.time)
+                .padding()
+            
+            List(viewModel.users) { user in
+                Text(user.name)
+            }
         }
     }
     
