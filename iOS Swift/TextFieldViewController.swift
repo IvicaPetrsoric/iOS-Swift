cf5a6615bffadc1845756fca1166da71e687c359
Ivica Petrsoric <ivica.petrsoric@gmail.com>
2024-03-05 23:24:46 +0000

    Setup custom text field

--- iOS Swift/TextFieldViewController.swift
+++ iOS Swift/TextFieldViewController.swift
@@ -17,12 +17,27 @@ class TextFieldViewController: UIViewController {
         
 //        view.backgroundColor = .orange
         
-        customView.textFieldData = TextFieldData(type: .text, required: false, title: "Test title", placeholder: "Placeholder", errorTitle: "postoji error")
-        customView.setCustomBackgroundColor(.white)
+//        customView.textFieldData =
+//        
+//        
+//        customView.setCustomBackgroundColor(.white)
+        
+        customView.setupTextField(
+            with: TextFieldData(
+                type: .text,
+                required: false,
+                title: "Test title",
+                placeholder: "Placeholder",
+                errorTitle: "postoji error"
+            )
+        )
         
 //        customView.fra
     }
     
+    
+    // regular 80
+    // max 101
 }
 
 struct TextFieldData {
@@ -62,51 +77,110 @@ class CustomTextFieldView: UIView {
     }
 
     
-    var textFieldData: TextFieldData!
     
-    private let regularBoderColor = UIColor.black
-    private let focusedBorderColor = UIColor.green
-    private let errorTitleColor = UIColor.red
+    // border colors
+    private let errorColor = UIColor.red
+    
+    
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var titleRequiredLabel: UILabel!
+    @IBOutlet private weak var textFieldContainerView: UIView!
+    @IBOutlet private weak var inputTextField: UITextField!
+    @IBOutlet private weak var showContentButton: UIButton!
+    @IBOutlet private weak var errorLabel: UILabel!
 
-    
-    private lazy var textField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = textFieldData.placeholder
-//        textField.hide
-        return textField
-    }()
-    
-    private lazy var titleLabel: UILabel = {
-        let label = UILabel()
-        return label
-    }()
-    
     override class func awakeFromNib() {
         super.awakeFromNib()
+        
+    }
+    
+    private var textFieldData: TextFieldData?
+    
+    func setupTextField(with data: TextFieldData) {
+        self.textFieldData = data
+        
+        inputTextField.isSecureTextEntry = data.type == .password
+        showContentButton.isHidden = data.type == .text
+        
+        
+        
+        if data.type == .text {
+            
+        }
+        // password type
+        else {
+            
+        }
+        
+        
+        
+        textFieldContainerView.layer.cornerRadius = 8
+        textFieldContainerView.layer.borderColor = UIColor.textFieldActiveBorderColor.cgColor
+        textFieldContainerView.layer.borderWidth = 2
+        
+        inputTextField.delegate = self
+        inputTextField.textColor = .black
+        
+        let placeholderText = "Enter text here"
+        let attributes = [NSAttributedString.Key.foregroundColor: UIColor.lightGray] // Set the color you want
+        inputTextField.attributedPlaceholder = NSAttributedString(string: placeholderText, attributes: attributes)
+        
+        titleLabel.text = "test"
+        
     }
     
     func setCustomBackgroundColor(_ color: UIColor) {
-//        backgroundColor = color
-        
-        layer.cornerRadius = 8
-        clipsToBounds = true
 
-        layer.borderColor = regularBoderColor.cgColor
-        layer.borderWidth = 2
+    }
+
+    var isShown = false
+    
+    @IBAction func toggleShowTextFieldContent(_ sender: UIButton) {
+        isShown = !isShown
+        
+        inputTextField.isSecureTextEntry = isShown
+        
+        let imageName = isShown ? "passwordHide" : "passwordShow"
+        sender.setImage(UIImage(named: imageName), for: .normal)
     }
     
-    private func setupUI() {
-//        addSubview(textField)
-//        
-//        NSLayoutConstraint.activate([
-//        
-//        ])
+    func updateErrorLabel() {
         
-//        textField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16)
-//        textField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 16)
-//        textField.heightAnchor.constraint(equalToConstant: 52)
-//        textField.centerYAnchor.constraint(equalTo: centerYAnchor)
     }
     
+    func updateTitleLabel() {
+        
+    }
 
+}
+
+
+extension UIColor {
+    
+    static var textFieldActiveBorderColor: UIColor {
+        return UIColor(hex: 0x6CD57C)
+    }
+    
+    static var textFieldInactiveBorderColor: UIColor {
+        return UIColor(hex: 0xEEEEEF)
+    }
+    
+    
+    
+    convenience init(hex: Int, alpha: Int = 255) {
+        self.init(
+            red: CGFloat((hex >> 16) & 0xFF) / 255.0,
+            green: CGFloat((hex >> 8) & 0xFF) / 255.0,
+            blue: CGFloat(hex & 0xFF) / 255.0,
+            alpha: CGFloat(alpha) / 255.0)
+    }
+    
+}
+ 
+extension CustomTextFieldView: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
